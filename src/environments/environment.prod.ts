1bdbc9081b854bd3d0b3e50a3e3d907fef03dc37
learoy-led <marketing@learoyled.com>
2025-07-07 17:49:03 +0200

    update ignore folder

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-    production: true,
-    // API_URL: 'https://rotuloslearoy-api-production.up.railway.app/api/'
-    API_URL: 'https://rotuloslearoy-api.onrender.com/api/'
-   };
