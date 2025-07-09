fcd92c362c75340b98766f9417f41936f0c0b2f6
learoy-led <marketing@learoyled.com>
2025-07-09 16:14:00 +0200

    update server.ts

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import express from 'express';
 import { fileURLToPath } from 'node:url';
 import { dirname, join, resolve } from 'node:path';
 import bootstrap from './src/main.server';
+import https from 'https';
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
@@ -19,20 +20,30 @@ export function app(): express.Express {
 
   // Example Express Rest API endpoints
   // server.get('/api/**', (req, res) => { });
-  // Serve static files from /browser
-  
-
 server.get('/sitemap.xml', async (req, res) => {
   try {
-    const response = await fetch('https://rotuloslearoy-api.onrender.com/api/sitemap.xml');
-    const xml = await response.text();
-    res.header('Content-Type', 'application/xml').send(xml);
-  } catch (err) {
-    res.status(500).send('Error fetching sitemap');
+    const apiUrl = 'https://rotuloslearoy-api.onrender.com/api/sitemap.xml';
+
+    https.get(apiUrl, (apiRes) => {
+      if (apiRes.statusCode !== 200) {
+        res.status(apiRes.statusCode ?? 500).send('Error al obtener sitemap');
+        return;
+      }
+
+      res.setHeader('Content-Type', 'application/xml');
+      apiRes.pipe(res); 
+    }).on('error', (err) => {
+      console.error('Error al obtener sitemap:', err);
+      res.status(500).send('Error interno');
+    });
+
+  } catch (error) {
+    console.error('Fallo al servir sitemap:', error);
+    res.status(500).send('Error interno del servidor');
   }
 });
 
-
+ // Serve static files from /browser
   server.get('**', express.static(browserDistFolder, {
     maxAge: '1y',
     index: 'index.html',
