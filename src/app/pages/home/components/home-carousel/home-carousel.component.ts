8dffff930826d657366f856588fc675978e9b4bb
learoy-led <marketing@learoyled.com>
2025-07-16 12:37:43 +0200

    line-clamp added to home category cards

--- src/app/pages/home/components/home-carousel/home-carousel.component.ts
+++ src/app/pages/home/components/home-carousel/home-carousel.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
 import { carouselImages } from '../../../../data/data';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from '../../../../shared/button/button.component';
+import { PlatformService } from '../../../../core/services/platform.service';
 
 @Component({
     selector: 'app-home-carousel',
@@ -10,13 +11,26 @@ import { ButtonComponent } from '../../../../shared/button/button.component';
     templateUrl: './home-carousel.component.html',
     styleUrl: './home-carousel.component.css'
 })
-export class HomeCarouselComponent {
+export class HomeCarouselComponent implements AfterViewInit {
 
   public imageIndex:number = 0
 public carouselImages: string[] = carouselImages
+@ViewChild('videoEl') videoEl!: ElementRef<HTMLVideoElement>;
+
+ constructor(private platformService: PlatformService) {}
 
   carouselImageChange(index:number) {
     this.imageIndex = index
   }
 
+  public ngAfterViewInit() {
+       if (this.platformService.isBrowser()) {
+     const video = this.videoEl.nativeElement;
+    video.muted = true;
+    video.play().catch(err => {
+      console.warn('Autoplay bloqueado por el navegador:', err);
+    });
+  }}
 }
+
+
