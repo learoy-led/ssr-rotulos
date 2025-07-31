011f3a983836eb8e407bbba2f2d19cd7ffc06c8c
learoy-led <marketing@learoyled.com>
2025-07-31 17:30:07 +0200

    delay and replace spinner icon

--- src/app/core/services/loading.service.ts
+++ src/app/core/services/loading.service.ts
@@ -9,6 +9,7 @@ export class LoadingService {
 
 private requests: HttpRequest<any>[] = [];
   private loadingSubject = new BehaviorSubject<boolean>(false);
+  private delayTimeout: any;
 
   public getLoadingStatus(): Observable<boolean> {
     return this.loadingSubject.asObservable();
@@ -16,14 +17,21 @@ private requests: HttpRequest<any>[] = [];
 
   public showLoading(request: HttpRequest<any>) {
     this.requests.push(request);
-    this.loadingSubject.next(true)
-
+    if (this.requests.length === 1) {
+    this.delayTimeout = setTimeout(() => {
+      this.loadingSubject.next(true);
+    }, 300);
+  }
   }
 
   public hideLoading(request: HttpRequest<any>) {
+
     const index = this.requests.indexOf(request);
+     if (index !== -1) {
     this.requests.splice(index, 1);
+      }
     if (this.requests.length === 0) {
+       clearTimeout(this.delayTimeout);
       this.loadingSubject.next(false)
     }
   }
