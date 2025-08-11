634b56b034fc9e8ce5cf0eb62d28606f317473d5
learoy-led <marketing@learoyled.com>
2025-08-11 16:40:02 +0200

    resolvers updated

--- src/app/core/resolvers/category.resolver.ts
+++ src/app/core/resolvers/category.resolver.ts
@@ -1,17 +1,29 @@
 import { inject } from '@angular/core';
-import { ResolveFn } from '@angular/router';
+import { ResolveFn, Router } from '@angular/router';
 import { GetProductsService } from '../services/get-products.service';
 import { catchError, map, of } from 'rxjs';
 import { Category } from '../../models/data.models';
 
-export const categoryResolver: ResolveFn<Category | { navigationCommands: any[] }> = (route, state) => {
+export const categoryResolver: ResolveFn<Category | null> = (route, state) => {
    const getProductsService =  inject(GetProductsService)
+   const router = inject(Router);
 
   const categorySlug = route.paramMap.get('category') ?? '';
+
   return getProductsService.getCategoryBySlug(categorySlug).pipe(
-    map(category => category ?? { navigationCommands: ['/pagina-no-encontrada'] }),
-    catchError(() => of({ navigationCommands: ['/pagina-no-encontrada'] }))
+    map(category => {
+      if (!category) {
+        router.navigate(['/pagina-no-encontrada']);
+        return null;
+      }
+      return category;
+    }),
+    catchError(() => {
+      router.navigate(['/pagina-no-encontrada']);
+      return of(null);
+    })
   );
+
   }
 
 
--- src/app/core/resolvers/product.resolver.ts
+++ src/app/core/resolvers/product.resolver.ts
@@ -3,14 +3,26 @@ import { GetProductsService } from '../services/get-products.service';
 import { inject } from '@angular/core';
 import { catchError, map, of } from 'rxjs';
 import { Product } from '../../models/data.models';
+import { Router } from 'express';
 
-export const productResolver: ResolveFn<Product | { navigationCommands: any[] }> = (route, state) => {
+export const productResolver: ResolveFn<Product | null > = (route, state) => {
   const getProductsService =  inject(GetProductsService)
+  const router = inject(Router);
 
   const productSlug = route.paramMap.get('product') ?? '';
- return getProductsService.getProductBySlug(productSlug).pipe(
-    map(product => product ?? { navigationCommands: ['/pagina-no-encontrada'] }),
-    catchError(() => of({ navigationCommands: ['/pagina-no-encontrada'] }))
+ 
+    return getProductsService.getProductBySlug(productSlug).pipe(
+    map(product => {
+      if (!product) {
+        router.navigate(['/pagina-no-encontrada']);
+        return null; 
+      }
+      return product;
+    }),
+    catchError(() => {
+      router.navigate(['/pagina-no-encontrada']);
+      return of(null);
+    })
   );
   
 };
