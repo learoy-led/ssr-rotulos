f00cc36fd5870a26845a58fca74e2c05ac5fcc08
learoy-led <marketing@learoyled.com>
2026-06-04 14:28:24 +0200

    nosotros

--- src/app/pages/nosotros/nosotros.component.ts
+++ src/app/pages/nosotros/nosotros.component.ts
@@ -1,27 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 import { AboutParagraph } from '../../models/data.models';
 import { AltPipe } from '../../pipes/alt.pipe';
-import { SocialMediaComponent } from '../../core/components/social-media/social-media.component';
-import { SeoService } from '../../core/services/seo.service';
+import { SocialMediaComponent } from '../../core/components/social-media/social-media.component'
 import { AboutService } from '../../services/about.service';
+import { Observable } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
     selector: 'app-nosotros',
       standalone: true,
-    imports: [AltPipe, SocialMediaComponent],
+    imports: [AltPipe, SocialMediaComponent, CommonModule],
     templateUrl: './nosotros.component.html',
     styleUrl: './nosotros.component.css'
 })
 export class NosotrosComponent implements OnInit {
 
-  public paragraphs: AboutParagraph[] = [];
+  public paragraphs$?: Observable <AboutParagraph[]>;
 
 constructor(private aboutService: AboutService) {}
 
   ngOnInit() {
-this.aboutService.getParagraphs().subscribe(data => {
-  this.paragraphs = data;
-});  
+this.paragraphs$ = this.aboutService.getParagraphs()
 }
 
 }
