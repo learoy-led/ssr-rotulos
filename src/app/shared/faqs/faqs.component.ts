0e280d039fcc99183d8ea452e0df52ec03ce8ebf
learoy-led <marketing@learoyled.com>
2026-07-31 17:54:56 +0200

    add functionallity to faqs

--- src/app/shared/faqs/faqs.component.ts
+++ src/app/shared/faqs/faqs.component.ts
@@ -1,7 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 import { faqs } from '../../data/faqs';
-import { Faq, Variable } from '../../models/data.models';
+import { ContactDetails, Faq, Variable } from '../../models/data.models';
 import { contactDetails } from '../../data/data';
 
 @Component({
@@ -10,14 +10,16 @@ import { contactDetails } from '../../data/data';
   templateUrl: './faqs.component.html',
   styleUrl: './faqs.component.css'
 })
-export class FaqsComponent implements OnInit {
+export class FaqsComponent implements OnChanges {
 
   @Input() material!: string
 
   public faqs: Faq[] = faqs
   public faqsProcessed: Faq[] = []
-  public contactDetails = contactDetails
-
+  public contactDetails: ContactDetails = contactDetails
+  public showAnswear: boolean = false
+  public openIndex: number | null = null;
+  public faqIsHidden: boolean =  false
 
 get variables(): Variable {
   return {
@@ -27,14 +29,24 @@ get variables(): Variable {
   };
 }
 
- ngOnInit(): void { 
-  
-     this.faqsProcessed = faqs.map(faq => ({
+ ngOnChanges(): void { 
+
+  this.faqsProcessed = this.faqs
+  .filter(faq => {
+    if (faq.question.includes('instala') && this.material !== 'neon') {
+      return false; 
+    }
+    return true;
+  }).map(faq => ({
         ...faq,
         question: faq.question.replace(/\{(\w+)\}/g, (_, key: keyof Variable) => this.variables[key] ?? ''),
         answear: faq.answear.replace(/\{(\w+)\}/g, (_, key: keyof Variable) => this.variables[key] ?? '')
       }));
-          
+        
+    }
+
+    public answearToggle(index: number) {
+this.openIndex = this.openIndex === index ? null : index;
     }
 
   }
